c175442b4c7662d6f35bef2afc21d52bdcfb77d2
206 <206@academicos.cesde.edu.co>
2022-10-03 12:18:37 -0500

    carpeta de service y zonas

---
+++ apiCuartosTcc/src/main/java/com/tcc/apiCuartosTcc/ApiCuartosTccApplication.java
@@ -0,0 +1,14 @@
+package com.tcc.apiCuartosTcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCuartosTccApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ApiCuartosTccApplication.class, args);
+	}
+
+}
